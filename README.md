2773672328d56e7f7a81c6972335b3dc890cee6c
JakobJK <jakobjk@gmail.com>
2020-12-03 17:07:53 -0500

    fixed an S from bad copy pasta, and added quick gif demo of the script running

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 locPlacement is a utility script for Maya, that will automatically position selected locators at the nearest geometry, from the viewpoint of the currently active camera. Helpful for especially during tracking/matchmoving
 
 ---
+![locPlacement](https://i.imgur.com/ZWy5Zgb.gif)
 
 ## Usage
 
@@ -15,10 +16,10 @@ import locPlacement
 locPlacement.main()
 ```
 
-## Author
+### Author
 
 [**Jakob Kousholt**](https://www.linkedin.com/in/jakejk/)
 
-## License
+### License
 
 locPlacement is licensed under the [MIT](https://rem.mit-license.org/) License.
--- locPlacement.py
+++ locPlacement.py
@@ -2,6 +2,7 @@ from maya import OpenMaya, cmds, OpenMayaUI
 import math
 import pymel.core as p
 
+
 def returnActiveCamera():
     view = OpenMayaUI.M3dView.active3dView()
     dagCamera = OpenMaya.MDagPath()
@@ -26,7 +27,8 @@ def RayIntersect(point, direction):
         item.extendToShape()
         fnMesh = OpenMaya.MFnMesh(item)
         raySource = OpenMaya.MFloatPoint(point[0], point[1], point[2], 1.0)
-        rayDir = OpenMaya.MFloatVector(direction[0], direction[1], direction[2])
+        rayDir = OpenMaya.MFloatVector(
+            direction[0], direction[1], direction[2])
         faceIds = None
         triIds = None
         idsSorted = False
@@ -43,8 +45,8 @@ def RayIntersect(point, direction):
         hitBarys2 = None
         tolerance = 0.0001
         fnMesh.allIntersections(raySource, rayDir, faceIds, triIds, idsSorted, worldSpace, maxParam,
-                                          testBothDirections, accelParams, sortHits, hitPoints, hitRayParams, hitFaces,
-                                          hitTris, hitBarys1, hitBarys2, tolerance)
+                                testBothDirections, accelParams, sortHits, hitPoints, hitRayParams, hitFaces,
+                                hitTris, hitBarys1, hitBarys2, tolerance)
         try:
             result.append([hitPoints[0].x, hitPoints[0].y, hitPoints[0].z])
         except:
@@ -56,12 +58,12 @@ def RayIntersect(point, direction):
 def findShortestDistance(original, arrOfPositions):
 
     shortestDistance = float('inf')
-    position = [0,0,0]
+    position = [0, 0, 0]
     for pos in arrOfPositions:
         dx = original[0] - pos[0]
         dy = original[1] - pos[1]
         dz = original[2] - pos[2]
-        newDistance = math.sqrt( dx*dx + dy*dy + dz*dz )
+        newDistance = math.sqrt(dx*dx + dy*dy + dz*dz)
         if (shortestDistance > newDistance):
             shortestDistance = newDistance
             position = pos
@@ -82,10 +84,11 @@ def run(loc):
     wsTransform = cmds.xform(cam, q=1, ws=1, rp=1, a=1)
     direction = getVectorBetweenTwoPoints(cam, loc)
     intersections = RayIntersect(wsTransform, direction)
-    if(len(intersections) == 0):S
+    if(len(intersections) == 0):
         print('No snappy for {0}'.format(loc))
     if(len(intersections) == 1):
-        cmds.move(intersections[0][0],intersections[0][1],intersections[0][2], loc, absolute=True)
+        cmds.move(intersections[0][0], intersections[0]
+                  [1], intersections[0][2], loc, absolute=True)
     if(len(intersections) > 1):
         locPosition = p.xform(loc, q=True, ws=True, t=True)
         shortDis = findShortestDistance(locPosition, intersections)
